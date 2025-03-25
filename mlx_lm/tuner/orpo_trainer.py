f04ae963c45c2067afbf46ff210c235ed339f9ad
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2025-03-25 16:16:30 +0100

    nits

--- mlx_lm/tuner/orpo_trainer.py
+++ mlx_lm/tuner/orpo_trainer.py
@@ -71,7 +71,7 @@ def orpo_loss(
         "chosen_logits_mean": mx.mean(chosen_logits_mean),
     }
 
-    mx.metal.clear_cache()
+    mx.clear_cache()
     return mx.mean(loss), reward, num_tokens, metrics
 
 
