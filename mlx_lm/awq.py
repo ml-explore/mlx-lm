51b6530283973d291831fbf845d7cb282a820532
Alex Barron <abarron22@apple.com>
2025-04-09 16:38:50 -0700

    add MOE support

--- mlx_lm/awq.py
+++ mlx_lm/awq.py
@@ -1,35 +1,62 @@
-# Learned quantization using AWQ:
+# Learned quantization using AWQ
 
 # References:
-# AWQ
 # https://arxiv.org/abs/2306.00978
 # https://github.com/mit-han-lab/llm-awq
 
 import argparse
 import glob
 import shutil
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable
 
 import mlx.core as mx
 import mlx.nn as nn
-import numpy as np
-from datasets import Dataset, load_dataset
-from mlx.utils import tree_flatten, tree_map_with_path
-from mlx_lm.models.base import create_attention_mask
-from mlx_lm.tokenizer_utils import TokenizerWrapper
-from mlx_lm.utils import fetch_from_hub, get_model_path, save_config, save_weights
+from datasets import load_dataset
+from mlx.utils import tree_flatten, tree_map, tree_map_with_path
 from tqdm import tqdm
+
+from mlx_lm.models.base import create_attention_mask
+from mlx_lm.models.switch_layers import SwitchLinear
+from mlx_lm.tokenizer_utils import TokenizerWrapper
+from mlx_lm.utils import (
+    MODEL_REMAPPING,
+    fetch_from_hub,
+    get_model_path,
+    save_config,
+    save_weights,
+)
+
+SUPPORTED_MODEL_TYPES = {
+    "llama",
+    "llama4",
+    "deepseek_v3",
+    "qwen2",
+    "gemma3",
+    "gemma3_text" "gemma2",
+}
 
 
 def mse(x, y):
     return ((x - y).astype(mx.float32)) ** 2
 
 
-def run_layer(layer: nn.Module, x: mx.array, batch_size: int = 32, **kwargs):
+def run_layer(
+    layer: nn.Module,
+    x: mx.array,
+    indices: mx.array | None = None,
+    batch_size: int = 32,
+    **kwargs,
+):
     y = []
     for i in range(0, x.shape[0], batch_size):
-        y.append(layer(x[i : i + batch_size], **kwargs))
+        if indices is not None:
+            y.append(
+                layer(x[i : i + batch_size], indices[i : i + batch_size], **kwargs)
+            )
+        else:
+            y.append(layer(x[i : i + batch_size], **kwargs))
         mx.eval(y)
     y = mx.concatenate(y, axis=0)
     return y
@@ -46,14 +73,18 @@ def dist_split(x: mx.array, group: mx.distributed.Group):
 
 def search_best_scale(
     layers: list[nn.Module],
-    x: mx.array,
     quantize_func: Callable,
     block: nn.Module | None = None,
     layer_kwargs: dict | None = None,
     n_grid: int = 20,
 ):
-    group = mx.distributed.init() if mx.distributed.is_available() else None
+    group = mx.distributed.init()
+
     layer_kwargs = layer_kwargs or {}
+
+    x = layers[0].input_feat
+    if (indices := layers[0].indices) is not None:
+        layer_kwargs["indices"] = indices
 
     block = block or layers[0]
     out = block(x, **layer_kwargs)
@@ -65,7 +96,9 @@ def search_best_scale(
 
     weights = tree_flatten(block.parameters())
 
-    for ratio in tqdm(range(n_grid)):
+    # Search across different scaling ratios
+    # and take the best loss.
+    for ratio in range(n_grid):
         ratio = ratio * 1 / n_grid
         scales = mx.maximum(x_max**ratio, 1e-4).reshape(-1)
         scales = scales / (scales.max() * scales.min()).sqrt()
@@ -92,8 +125,8 @@ def search_best_scale(
 
 
 def apply_scale(prev_op, layers, scales):
-    # Apply the scales to the layers
-    if isinstance(prev_op, nn.Linear):
+    # Fuse the scales into the previous op
+    if isinstance(prev_op, (nn.Linear, SwitchLinear)):
         assert len(layers) == 1
         prev_op.weight = prev_op.weight / scales[:, mx.newaxis]
         if hasattr(prev_op, "bias"):
@@ -105,59 +138,102 @@ def apply_scale(prev_op, layers, scales):
             prev_op.bias = prev_op.bias / scales
         for layer in layers:
             layer.weight = layer.weight * scales
+    elif prev_op.__class__.__name__ == "RMSNorm":  # For gemma models
+        prev_op.weight = (1.0 + prev_op.weight) / scales - 1.0
+        for layer in layers:
+            layer.weight = layer.weight * scales
     else:
         raise NotImplementedError(f"Could not apply scale to prev_op: {prev_op}")
 
+    for layer in layers:
+        layer.input_feat = layer.input_feat / scales
+
+
+@dataclass
+class LayerConfig:
+    prev_op: nn.Module
+    layers: list[nn.Module]
+    block: nn.Module | None = None
+    layer_kwargs: dict | None = None
+
 
 def scale_block(
-    block, input_feat, quantize_func: Callable, layer_kwargs: dict | None = None
+    block: nn.Module, mask: mx.array, quantize_func: Callable, n_grid: int = 20
 ):
-    layers = [
-        block.self_attn.q_proj,
-        block.self_attn.k_proj,
-        block.self_attn.v_proj,
-    ]
-    scales = search_best_scale(
-        layers=layers,
-        block=block.self_attn,
-        x=input_feat["q_proj"],
-        quantize_func=quantize_func,
-        layer_kwargs=layer_kwargs,
-    )
-    apply_scale(block.input_layernorm, layers, scales)
-    for name in ["q_proj", "k_proj", "v_proj"]:
-        input_feat[name] = input_feat[name] / scales
-
-    layers = [
-        block.mlp.gate_proj,
-        block.mlp.up_proj,
-    ]
-    scales = search_best_scale(
-        block=block.mlp,
-        layers=layers,
-        x=input_feat["gate_proj"],
-        quantize_func=quantize_func,
-    )
+    # Layers which have the same inputs (within an elementwise multiplication)
+    # can be scaled together.
     mlp_norm = getattr(
         block, "pre_feedforward_layernorm", block.post_attention_layernorm
     )
-    apply_scale(mlp_norm, layers, scales)
-    for name in ["gate_proj", "up_proj"]:
-        input_feat[name] = input_feat[name] / scales
-
-    layers = [block.mlp.down_proj]
-    scales = search_best_scale(
-        layers=layers,
-        x=input_feat["down_proj"],
-        quantize_func=quantize_func,
-    )
-    apply_scale(block.mlp.up_proj, layers, scales)
-    input_feat["down_proj"] = input_feat["down_proj"] / scales
+    config = [
+        LayerConfig(
+            block=block.self_attn,
+            prev_op=block.input_layernorm,
+            layers=[
+                block.self_attn.q_proj,
+                block.self_attn.k_proj,
+                block.self_attn.v_proj,
+            ],
+            layer_kwargs={"mask": mask},
+        )
+    ]
+    # Llama 4 MOE layer
+    if hasattr(block, "feed_forward") and block.is_moe_layer:
+        config += [
+            LayerConfig(
+                block=block.feed_forward,
+                prev_op=block.post_attention_layernorm,
+                layers=[
+                    block.feed_forward.shared_expert.gate_proj,
+                    block.feed_forward.shared_expert.up_proj,
+                    block.feed_forward.experts.gate_proj,
+                    block.feed_forward.experts.up_proj,
+                    block.feed_forward.router,
+                ],
+            ),
+            LayerConfig(
+                prev_op=block.feed_forward.experts.up_proj,
+                layers=[
+                    block.feed_forward.experts.down_proj,
+                ],
+            ),
+            LayerConfig(
+                prev_op=block.feed_forward.shared_expert.up_proj,
+                layers=[
+                    block.feed_forward.shared_expert.down_proj,
+                ],
+            ),
+        ]
+    else:
+        config += [
+            LayerConfig(
+                block=block.mlp,
+                prev_op=mlp_norm,
+                layers=[
+                    block.mlp.gate_proj,
+                    block.mlp.up_proj,
+                ],
+            ),
+            LayerConfig(
+                prev_op=block.mlp.up_proj,
+                layers=[
+                    block.mlp.down_proj,
+                ],
+            ),
+        ]
+    for conf in config:
+        scales = search_best_scale(
+            layers=conf.layers,
+            block=conf.block,
+            layer_kwargs=conf.layer_kwargs,
+            quantize_func=quantize_func,
+            n_grid=n_grid,
+        )
+        apply_scale(conf.prev_op, conf.layers, scales)
 
 
 def search_best_clip(
-    w: mx.array,
-    x: mx.array,
+    module: nn.Module,
     quantize_func: Callable,
     group_size: int,
     n_grid: int = 20,
@@ -165,7 +241,10 @@ def search_best_clip(
     subsample: int = 4,
     batch_size: int = 64,
 ):
-    group = mx.distributed.init() if mx.distributed.is_available() else None
+    group = mx.distributed.init()
+
+    x = module.input_feat
+    w = module.weight
 
     x = x[:, ::subsample]
     x = x.reshape(*x.shape[:-1], -1, group_size)
@@ -174,6 +253,7 @@ def search_best_clip(
     w_max_all = []
     w_min_all = []
 
+    # batch across W to save memory
     for b in range(0, w.shape[0], batch_size):
         w = w_all[b : b + batch_size]
 
@@ -187,6 +267,7 @@ def search_best_clip(
         w = w.reshape(*w.shape[:-1], -1, group_size)
         out = mx.einsum("btdg,odg->btod", x, w)
 
+        # try a range of clips and pick the one with the smallest loss
         for i in range(int(max_shrink * n_grid)):
             p = 1 - i / n_grid
             w_max = p * w.max(axis=-1, keepdims=True)
@@ -224,10 +305,7 @@ def search_best_clip(
 
 
 def clip_block(
-    block: nn.Module,
-    input_feat: dict[str, mx.array],
-    quantize_func: Callable,
-    group_size: int,
+    block: nn.Module, quantize_func: Callable, group_size: int, n_grid: int = 20
 ):
 
     def apply_clip(path, module):
@@ -236,12 +314,11 @@ def clip_block(
             and "q_proj" not in path
             and "k_proj" not in path
         ):
-            name = path.split(".")[-1]
             best_weight = search_best_clip(
-                module.weight,
-                input_feat[name],
+                module,
                 quantize_func=quantize_func,
                 group_size=group_size,
+                n_grid=n_grid,
             )
             module.weight = best_weight
 
@@ -255,8 +332,9 @@ def awq_quantize(
     bits: int = 3,
     embed_group_size: int = 32,
     embed_bits: int = 4,
+    n_grid: int = 20,
 ):
-    group = mx.distributed.init() if mx.distributed.is_available() else None
+    group = mx.distributed.init()
 
     def quantize_func(w):
         wq = mx.quantize(w, bits=bits, group_size=group_size)
@@ -269,78 +347,83 @@ def awq_quantize(
     )
     inputs = model.model.embed_tokens(inputs)
 
-    input_feat = {}
-
-    def capture(path, module):
-        if not isinstance(module, nn.Linear):
+    def capture(module):
+        if not isinstance(module, (nn.Linear, SwitchLinear)):
             return module
 
         class Catcher(nn.Module):
-            def __call__(self, x: mx.array):
-                name = path.split(".")[-1]
-                input_feat[name] = x
-                return module(x)
+            def __call__(self, x: mx.array, *args, **kwargs):
+                # Store the input features on the original modules.
+                if hasattr(module, "input_feat"):
+                    module.input_feat = mx.concatenate([module.input_feat, x], axis=0)
+                else:
+                    module.input_feat = x
+
+                # Also store the MOE indices if applicabale
+                module.indices = None
+                if isinstance(module, SwitchLinear):
+                    indices = args[0]
+                    if module.indices is not None:
+                        module.indices = mx.concatenate(
+                            [module.indices, indices], axis=0
+                        )
+                    else:
+                        module.indices = indices
+
+                return module(x, *args, **kwargs)
 
         return Catcher()
 
-    for i, layer in enumerate(model.model.layers):
-        import time
-
-        s = time.time()
-        print(f"Starting block {i}")
-
-        # capture the inputs to each layer
-        orig_leaves = layer.leaf_modules()
-        capture_leaves = tree_map_with_path(
-            capture, orig_leaves, is_leaf=nn.Module.is_module
-        )
-        layer.update_modules(capture_leaves)
-        outputs = run_layer(layer, inputs, mask=mask)
-        layer.update_modules(orig_leaves)
+    for block in tqdm(model.model.layers):
+        # Capture the input features for each of the layers in the transformer block
+        orig_leaves = block.leaf_modules()
+        capture_leaves = tree_map(capture, orig_leaves, is_leaf=nn.Module.is_module)
+        block.update_modules(capture_leaves)
+        outputs = run_layer(block, inputs, mask=mask)
+        block.update_modules(orig_leaves)
         del capture_leaves
 
-        nn.quantize(layer, group_size=group_size, bits=bits)
-        outputs_q = run_layer(layer, inputs, mask=mask)
-        loss = mse(outputs, outputs_q).sum()
+        # Quantize the block without AWQ to obtain a reference loss
+        nn.quantize(block, group_size=group_size, bits=bits)
+        outputs_q = run_layer(block, inputs, mask=mask)
+        before_loss = mse(outputs, outputs_q).sum()
         if group is not None:
-            loss = mx.distributed.all_sum(loss, stream=mx.cpu) / group.size()
-        loss /= outputs.size
-        print("Before Loss", loss, flush=True)
-        layer.update_modules(orig_leaves)
+            before_loss = (
+                mx.distributed.all_sum(before_loss, stream=mx.cpu) / group.size()
+            )
+        before_loss /= outputs.size
+        block.update_modules(orig_leaves)
         del orig_leaves
 
-        print("Scaling block", flush=True)
         scale_block(
-            block=layer,
-            input_feat=input_feat,
+            block=block,
+            mask=mask,
             quantize_func=quantize_func,
-            layer_kwargs={"mask": mask},
+            n_grid=n_grid,
         )
 
-        print("Clipping block", flush=True)
         clip_block(
-            block=layer,
-            input_feat=input_feat,
+            block=block,
             quantize_func=quantize_func,
             group_size=group_size,
+            n_grid=n_grid,
         )
 
-        nn.quantize(layer, group_size=group_size, bits=bits)
-        outputs_q = run_layer(layer, inputs, mask=mask)
-        loss = mse(outputs, outputs_q).sum()
+        # Quantize the scaled and clipped block
+        nn.quantize(block, group_size=group_size, bits=bits)
+        outputs_q = run_layer(block, inputs, mask=mask)
+        after_loss = mse(outputs, outputs_q).sum()
         if group is not None:
-            loss = mx.distributed.all_sum(loss, stream=mx.cpu) / group.size()
-        loss /= outputs.size
-        print("After Loss", loss, flush=True)
+            after_loss = (
+                mx.distributed.all_sum(after_loss, stream=mx.cpu) / group.size()
+            )
+        after_loss /= outputs.size
+        tqdm.write(f"Loss reduction: {after_loss / before_loss}")
 
-        input_feat = {}
         inputs = outputs
 
-        mx.eval(layer)
+        mx.eval(block)
         mx.metal.clear_cache()
-
-        e = time.time()
-        print("Loop time: ", e - s)
 
     if hasattr(model, "lm_head"):
         model.lm_head = model.lm_head.to_quantized(
@@ -362,7 +445,7 @@ def load_wikitext(
     data = tokens[starts + mx.arange(sequence_length)]
     if tokenizer.bos_token_id:
         data = mx.concatenate(
-            [mx.full((*data.shape[:2], 1), tokenizer.bos_token_id), data], axis=-1
+            [mx.full((*data.shape[:1], 1), tokenizer.bos_token_id), data], axis=-1
         )
     return data
 
@@ -409,12 +492,15 @@ def main():
     parser.add_argument("--mlx-path", default="mlx_model")
     parser.add_argument("--bits", type=int, default=3)
     parser.add_argument("--group-size", type=int, default=64)
+    parser.add_argument("--embed-bits", type=int, default=4)
+    parser.add_argument("--embed-group-size", type=int, default=32)
     parser.add_argument("--num-samples", type=int, default=32)
     parser.add_argument("--sequence-length", type=int, default=2048)
+    parser.add_argument("--n-grid", type=int, default=10)
     parser.add_argument("--seed", type=int, default=123)
     args = parser.parse_args()
 
-    group = mx.distributed.init() if mx.distributed.is_available() else None
+    group = mx.distributed.init()
 
     num_samples = args.num_samples
     if group is not None and num_samples % group.size() > 0:
@@ -425,12 +511,27 @@ def main():
     model_path = get_model_path(args.model, revision=None)
     model, config, tokenizer = fetch_from_hub(model_path, lazy=True)
 
+    model_type = config["model_type"]
+    model_type = MODEL_REMAPPING.get(model_type, model_type)
+    if model_type not in SUPPORTED_MODEL_TYPES:
+        raise NotImplementedError(f"AWQ support for {config['model_type']} models NYI.")
+
     calibration_data = load_wikitext(tokenizer, args.num_samples, args.sequence_length)
 
     if group is not None:
         calibration_data = dist_split(calibration_data, group)
 
-    awq_quantize(model, calibration_data, bits=args.bits, group_size=args.group_size)
+    # For Gemma3 vision/text models
+    language_model = getattr(model, "language_model", model)
+    awq_quantize(
+        language_model,
+        calibration_data,
+        bits=args.bits,
+        group_size=args.group_size,
+        embed_bits=args.embed_bits,
+        embed_group_size=args.embed_group_size,
+        n_grid=args.n_grid,
+    )
 
     save_model(model, tokenizer, config, model_path, args.mlx_path)
 
