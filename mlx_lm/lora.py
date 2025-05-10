5713919983522f07f623a7b8f6c4160ffafc855c
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2025-05-11 00:20:44 +0200

    use CacheDataset()

--- mlx_lm/lora.py
+++ mlx_lm/lora.py
@@ -292,8 +292,8 @@ def train_model(
         train_orpo(
             model=model,
             optimizer=opt,
-            train_dataset=train_set,
-            val_dataset=valid_set,
+            train_dataset=CacheDataset(train_set),
+            val_dataset=CacheDataset(valid_set),
             args=training_args,
             training_callback=training_callback,
         )
@@ -314,8 +314,8 @@ def train_model(
             model=model,
             args=training_args,
             optimizer=opt,
-            train_dataset=train_set,
-            val_dataset=valid_set,
+            train_dataset=CacheDataset(train_set),
+            val_dataset=CacheDataset(valid_set),
             training_callback=training_callback,
         )
 
