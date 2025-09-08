e4841dd6980415041ae6986226d1ecd7c37a635b
Goekdeniz-Guelmez <gulmezdeniz1999@gmail.com>
2025-09-08 17:20:23 +0200

    nits

--- mlx_lm/models/Klear.py
+++ mlx_lm/models/Klear.py
@@ -1,7 +1,7 @@
 # Copyright © 2025 Apple Inc.
 
 from dataclasses import dataclass
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, List, Optional
 
 import mlx.core as mx
 import mlx.nn as nn
