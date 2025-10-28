add metal wkv kernel and fix groupnorm calculation
Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

mlx_lm/models/rwkv7.py
@@ -24,12 +24,6 @@ class ModelArgs(BaseModelArgs):
     decay_low_rank_dim: int
     tie_word_embeddings: bool = True
 
-    def __post_init__(self):
-        if not hasattr(self, "num_hidden_layers") and hasattr(self, "n_layer"):
-            self.num_hidden_layers = self.n_layer
-        if not hasattr(self, "num_hidden_layers") and hasattr(self, "n_layers"):
-            self.num_hidden_layers = self.n_layers
-
 
 @partial(mx.compile, shapeless=True)
 def addcmul(x, y, z):
@@ -47,6 +41,123 @@ def _wkv7_step_ops(r, w, k, v, a, b, state):
     state = state * w[:, :, None, :] + v[..., None] @ k[..., None, :] + sab
     y = state @ r[..., None]
     return y, state
+
+
+def _make_wkv7_kernel():
+    if not mx.metal.is_available():
+        return None
+    source = f"""
+        auto n = thread_position_in_grid.z;
+        auto b_idx = n / H;
+        auto h_idx = n % H;
+        constexpr int n_per_t = D / 32;
+
+        // [B, T, H, D]
+        auto r_ = r + b_idx * T * H * D + h_idx * D;
+        auto w_ = w + b_idx * T * H * D + h_idx * D;
+        auto k_ = k + b_idx * T * H * D + h_idx * D;
+        auto v_ = v + b_idx * T * H * D + h_idx * D;
+        auto a_ = a + b_idx * T * H * D + h_idx * D;
+        auto b_ = b + b_idx * T * H * D + h_idx * D;
+        y += b_idx * T * H * D + h_idx * D;
+
+        auto dk_idx = thread_position_in_threadgroup.x;
+        auto dv_idx = thread_position_in_grid.y;
+
+        // state_in, state_out: [B, H, D, D]
+        auto i_state = state_in  + (n * D + dv_idx) * D;
+        auto o_state = state_out + (n * D + dv_idx) * D;
+
+        float state[n_per_t];
+        for (int i = 0; i < n_per_t; ++i) {{
+          auto s_idx = n_per_t * dk_idx + i;
+          state[i] = static_cast<float>(i_state[s_idx]);
+        }}
+
+        for (int t = 0; t < T; ++t) {{
+          float sa = 0.0f;
+          for (int i = 0; i < n_per_t; ++i) {{
+            auto s_idx = n_per_t * dk_idx + i;
+            sa += state[i] * a_[s_idx];
+            state[i] = state[i] * w_[s_idx];
+          }}
+          sa = simd_sum(sa);
+
+          float out = 0.0f;
+          for (int i = 0; i < n_per_t; ++i) {{
+            auto s_idx = n_per_t * dk_idx + i;
+            state[i] = state[i] + k_[s_idx] * v_[dv_idx] + sa * b_[s_idx];
+            out += state[i] * r_[s_idx];
+          }}
+          out = simd_sum(out);
+          if (thread_index_in_simdgroup == 0) {{
+            y[dv_idx] = static_cast<InT>(out);
+          }}
+
+          // Increment data pointers to next time step
+          r_ += H * D;
+          w_ += H * D;
+          k_ += H * D;
+          v_ += H * D;
+          a_ += H * D;
+          b_ += H * D;
+          y  += H * D;
+        }}
+        for (int i = 0; i < n_per_t; ++i) {{
+          auto s_idx = n_per_t * dk_idx + i;
+          o_state[s_idx] = static_cast<InT>(state[i]);
+        }}
+    """
+    inputs = ["r", "w", "k", "v", "a", "b", "state_in", "T"]
+    return mx.fast.metal_kernel(
+        name="wkv7_step",
+        input_names=inputs,
+        output_names=["y", "state_out"],
+        source=source,
+    )
+
+
+_wkv7_kernel = _make_wkv7_kernel()
+
+
+def wkv7_kernel(
+    r: mx.array,
+    w: mx.array,
+    k: mx.array,
+    v: mx.array,
+    a: mx.array,
+    b: mx.array,
+    state: mx.array,
+):
+    B, T, H, D = r.shape
+    input_dtype = r.dtype
+
+    return _wkv7_kernel(
+        inputs=[r, w, k, v, a, b, state, T],
+        template=[
+            ("InT", input_dtype),
+            ("H", H),
+            ("D", D),
+        ],
+        grid=(32, D, B * H),
+        threadgroup=(32, 4, 1),
+        output_shapes=[(B, T, H, D), state.shape],
+        output_dtypes=[input_dtype, input_dtype],
+    )
+
+
+class LayerNormPerHead(nn.Module):
+    def __init__(self, head_dim, num_heads, eps):
+        super().__init__()
+        # self.norms = [
+        #     nn.LayerNorm(head_dim, eps=eps, bias=False) for _ in range(num_heads)
+        # ]
+        self.weight = mx.zeros((num_heads, head_dim))
+        self.bias = mx.zeros((num_heads, head_dim))
+        self.eps = eps
+
+    def __call__(self, x):
+        return self.weight * mx.fast.layer_norm(x, None, None, self.eps) + self.bias
 
 
 class LoRA(nn.Module):
@@ -153,9 +264,7 @@ class Rwkv7TimeMixing(nn.Module):
             self.hidden_size, self.hidden_size, bias=False
         )
 
-        self.g_norm = nn.GroupNorm(
-            self.num_heads, self.hidden_size, eps=64e-5, affine=True, pytorch_compatible=True
-        )
+        self.g_norm = LayerNormPerHead(self.head_dim, self.num_heads, eps=64e-5)
 
         self.w_lora = LoRA(
             self.hidden_size, self.hidden_size, low_rank_dim=self.decay_low_rank_dim, activation='tanh'
@@ -173,27 +282,31 @@ class Rwkv7TimeMixing(nn.Module):
         self.g_lora = LoRA(
             self.hidden_size, self.hidden_size, low_rank_dim=self.gate_low_rank_dim, activation='sigmoid', bias=False
         )
-    
-    def _wkv7(self, r, w, k, v, a, b, state):
+
+    def _wkv7(self, r, w, k, v, a, b, state, use_kernel: bool = True):
         B, L, _, _ = r.shape
         if state is None:
-            state = mx.zeros((B, self.num_heads, self.head_dim, self.head_dim), dtype=mx.float32)
+            state = mx.zeros((B, self.num_heads, self.head_dim, self.head_dim), dtype=r.dtype)
 
-        ys = []
-        for t in range(L):
-            y, state = _wkv7_step_ops(
-                r[:, t],
-                w[:, t],
-                k[:, t],
-                v[:, t],
-                a[:, t],
-                b[:, t],
-                state
-            )
-            ys.append(y)
+        if not use_kernel or mx.default_device() != mx.gpu or not mx.metal.is_available():
+            ys = []
+            for t in range(L):
+                y, state = _wkv7_step_ops(
+                    r[:, t],
+                    w[:, t],
+                    k[:, t],
+                    v[:, t],
+                    a[:, t],
+                    b[:, t],
+                    state
+                )
+                ys.append(y)
 
-        y = mx.stack(ys, axis=1).astype(r.dtype)
-        return y, state
+            y = mx.stack(ys, axis=1).astype(r.dtype)
+            return y, state
+        else:
+            return wkv7_kernel(r, w, k, v, a, b, state)
+
 
     def __call__(self, x, v_first, cache):
         if cache is None:
@@ -223,15 +336,15 @@ class Rwkv7TimeMixing(nn.Module):
         else:
             value = value + (v_first - value) * mx.sigmoid(self.v_lora(xv)).reshape(B, L, self.num_heads, self.head_dim)
 
-        decay = mx.exp(-0.606531 * mx.sigmoid(self.w_lora(xw).reshape(B, L, self.num_heads, self.head_dim)).astype(mx.float32))
+        decay = mx.exp(-0.606531 * mx.sigmoid(self.w_lora(xw).reshape(B, L, self.num_heads, self.head_dim)).astype(mx.float32)).astype(receptance.dtype)
         kk = l2_norm((key * self.k_k))
         key = key * (1 + (a - 1) * self.k_a)
         b = kk * a
         a = -kk
 
         out, new_state_cache = self._wkv7(receptance, decay, key, value, a, b, state_cache)
-        out = self.g_norm(out.reshape([B, L, D]))
-        out = out + ((receptance * key * self.r_k).sum(axis=-1, keepdims=True) * value).reshape([B, L, D])
+        out = self.g_norm(out.reshape(B, L, self.num_heads, self.head_dim))
+        out = (out + ((receptance * key * self.r_k).sum(axis=-1, keepdims=True) * value)).reshape([B, L, D])
 
         if isinstance(cache, RwkvCache):
             cache[0] = x[:, -1:, :]
@@ -254,7 +367,7 @@ class Rwkv7Layer(nn.Module):
 
     def __call__(self, x, v_first, cache):
         if self.layer_idx == 0:
-            x = self.pre_norm(x)
+            x = self.pre_norm(x).astype(mx.float16)
 
         h, v_first = self.attn(self.attn_norm(x), v_first, cache)
         h = x + h
@@ -310,8 +423,8 @@ class Model(nn.Module):
 
     def sanitize(self, weights):
         for k, v in weights.items():
-            if v.dtype == mx.bfloat16:
+            if v.dtype == mx.bfloat16 and "embeddings" not in k:
                 weights[k] = v.astype(mx.float16)
-            if "k_k" in k or "k_a" in k:
+            if "k_k" in k or "k_a" in k or "g_norm" in k:
                 weights[k] = weights[k].reshape(self.args.hidden_size // self.args.head_dim, self.args.head_dim)
         return weights