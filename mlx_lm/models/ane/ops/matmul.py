c8bb9c710a562fa717bb1ac671ef6ae53723e3b8
yiakwy-xpu-ml-framework-team <961186938@qq.com>
2025-11-17 19:54:26 +0800

    add performance benchmark

--- mlx_lm/models/ane/ops/matmul.py
+++ mlx_lm/models/ane/ops/matmul.py
@@ -11,6 +11,9 @@ import coremltools.models.datatypes as datatypes
 try:
     import coremltools as ct
 
+    from coremltools.models.neural_network import quantization_utils
+    from coremltools.proto.FeatureTypes_pb2 import ArrayFeatureType
+
     from Cocoa import NSURL
     from CoreML import MLModel, MLModelConfiguration
     from CoreML import MLDictionaryFeatureProvider, MLFeatureValue
@@ -24,7 +27,7 @@ except Exception as e:
 def ane_subgraph_builder(x_desc : tuple, w : np.ndarray, b : np.ndarray = None, input_name="x", output_name="out", prefix : str = "") -> ct.models.MLModel:
     if w.ndim != 2:
         # reshape
-        pass
+        w = w.reshape(-1, w.shape[-1])
 
     M, K = x_desc
     N, K = w.shape
@@ -42,13 +45,18 @@ def ane_subgraph_builder(x_desc : tuple, w : np.ndarray, b : np.ndarray = None, 
     spec = builder.spec
     spec.description.predictedFeatureName = output_name
 
-    # ct.utils.convert_double_to_float_multiarray_type(spec)
+    ct.utils.convert_double_to_float_multiarray_type(spec)
 
-    model = ct.models.MLModel(spec)
-    return model, output_name
+    model_fp32 = ct.models.MLModel(spec)
+
+    # https://apple.github.io/coremltools/docs-guides/source/quantization-neural-network.html
+    model_fp16 = quantization_utils.quantize_weights(model_fp32, nbits=16, quantization_mode='linear',)
+
+    return model_fp16, output_name
 
 
 _cache = {}
+
 
 # TODO (yiakwy) : add multi-levels cache
 def _hash_matmul(W, b=None):
@@ -86,7 +94,7 @@ def matmul(x : np.ndarray, w : np.ndarray, b : np.ndarray = None, prefix : str =
             if dump:
                 save_model_proto(model, saved_path, model_name)
 
-            _cache[key] = (model)
+            _cache[key] = (model,)
         else:
             model = cached[0]
             output_name = f'{prefix}/{output_name}'
@@ -95,17 +103,17 @@ def matmul(x : np.ndarray, w : np.ndarray, b : np.ndarray = None, prefix : str =
     
     outputs = model.predict(inputs)
 
-    out = np.array(outputs[output_name], dtype=np.float32)
+    out = outputs[output_name]
     return out
 
 
 def test_fp8_group_scaled_gemm():
     test_configs = [
         # (8, 32, 8)
-        # (1, 1, 1)
         # (128, 256, 128)
-        (1024, 4096, 1024)
-        # (4096, 16384, 4096),
+        # (1024, 4096, 1024)
+        (4096, 16384, 4096),
+        # (32, 128, 1024)
     ]
 
     for M, K, N in test_configs:
@@ -140,6 +148,37 @@ def test_fp8_group_scaled_gemm():
         max_error = np.max(np.abs(output_torch.cpu().numpy() - output_ane))
         print(f"Max error between torch and mlx: {max_error:.6f}")
 
+        # print(f"diff : {output_ane - output_torch.cpu().numpy()}")
+        print(f"ane type : {output_ane.dtype}")
+
+        # Performance benchmark for ANE
+
+        import time
+
+        for _ in range(10):
+            _ = torch.matmul(input_fp16, weights_fp16)
+        torch.mps.synchronize()
+
+        for _ in range(10):
+            _ = matmul(input_fp16_np, weights_fp16_np)
+
+        # Benchmark Pytorch MPS backend
+        start_time = time.time()
+        for _ in range(10):
+            _ = torch.matmul(input_fp16, weights_fp16)
+        torch.mps.synchronize()
+        torch_elpase = (time.time() - start_time)/ 10 * 1000
+
+        # Benchmark for ANE
+        start_time = time.time()
+        for _ in range(10):
+            _ = matmul(input_fp16_np, weights_fp16_np)
+
+        mlx_elpase = (time.time() - start_time)/ 10 * 1000
+        
+        print(f"fp16x{M}x{N}x{K} Pytorch MPS backend average time over 10 runs: {torch_elpase:.2f} ms")
+        print(f"fp16x{M}x{N}x{K} ANE backend average time over 10 runs: {mlx_elpase:.2f} ms")
+        
 
 if __name__ == "__main__":
     test_fp8_group_scaled_gemm()
