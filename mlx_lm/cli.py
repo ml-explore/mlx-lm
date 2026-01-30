f4f669e80871c84d36816730f958c3c50552a91d
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2026-01-30 06:07:36 +0530

    fix cli

--- mlx_lm/cli.py
+++ mlx_lm/cli.py
@@ -25,7 +25,11 @@ def main():
     )
     if len(sys.argv) < 2:
         raise ValueError(f"CLI requires a subcommand in {subcommands}")
-    subcommand = sys.argv.pop(1)
+    subcommand = sys.argv.pop(1).replace("mlx_lm.", "")
+
+    if subcommand not in subcommands and subcommand not in ("--version", "-h", "--help"):
+        subcommand = next((s for s in subcommands if s.endswith(f".{subcommand}")), subcommand)
+
     if subcommand in subcommands:
         submodule = importlib.import_module(f"mlx_lm.{subcommand}")
         submodule.main()
@@ -34,11 +38,13 @@ def main():
 
         print(__version__)
     elif subcommand in ("-h", "--help"):
-        print(f"The supported subcommand are {subcommands}")
-        print()
+        print("The supported subcommands are:\n")
+        w = max(map(len, subcommands)) + 2
+        for i in range(0, len(subcommands), 3):
+            print(" ", *(f"{s:{w}}" for s in subcommands[i : i + 3]))
         print(
-            "For help on an individual subcommand, pass --help "
-            "to the subommand. For example: mlx_lm.generate --help"
+            "\nFor help on an individual subcommand, pass --help "
+            "to the subcommand. For example: mlx_lm.generate --help"
         )
     else:
         raise ValueError(f"CLI requires a subcommand in {subcommands}")
