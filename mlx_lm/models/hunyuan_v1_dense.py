b0c22bb0308382b8e81644d1aa6a6a13de9273aa
Goekdeniz-Guelmez <gulmezdeniz1999@gmail.com>
2026-03-06 18:56:58 +0100

    feat: add rope_parameters to ModelArgs and update rope_theta in post_init

--- mlx_lm/models/hunyuan_v1_dense.py
+++ mlx_lm/models/hunyuan_v1_dense.py
@@ -27,6 +27,11 @@ class ModelArgs(BaseModelArgs):
     rope_scaling: Optional[Dict[str, Union[float, str]]] = None
     tie_word_embeddings: bool = False
     head_dim: Optional[int] = None
+    rope_parameters: Optional[Dict] = None
+
+    def __post_init__(self):
+        if self.rope_parameters is not None:
+            self.rope_theta = self.rope_parameters.get("rope_theta", 10000.0)
 
     def __post_init__(self):
         if self.rope_scaling:
