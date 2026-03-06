0f65ca42ea28b7b02d5bc4c322dfb472f639f8f5
Goekdeniz-Guelmez <gulmezdeniz1999@gmail.com>
2026-03-06 18:55:56 +0100

    feat: add rope_parameters to ModelArgs and initialize rope_theta in post_init

--- mlx_lm/models/internlm3.py
+++ mlx_lm/models/internlm3.py
@@ -27,8 +27,12 @@ class ModelArgs(BaseModelArgs):
     rope_traditional: bool = False
     rope_scaling: Optional[Dict[str, Union[float, str]]] = None
     tie_word_embeddings: bool = False
+    rope_parameters: Optional[Dict] = None
 
     def __post_init__(self):
+        if self.rope_parameters is not None:
+            self.rope_theta = self.rope_parameters.get("rope_theta", 10000.0)
+
         if self.num_key_value_heads is None:
             self.num_key_value_heads = self.num_attention_heads
 
