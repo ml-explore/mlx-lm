55d29e248fdb7ee5e04c854f3d0e08dcd2898b64
Goekdeniz-Guelmez <gulmezdeniz1999@gmail.com>
2026-03-06 19:11:45 +0100

    feat: add rope_parameters to ModelArgs and initialize rope_theta in post_init

--- mlx_lm/models/apertus.py
+++ mlx_lm/models/apertus.py
@@ -1,7 +1,6 @@
 # Copyright © 2023-2025 Apple Inc.
 
 from dataclasses import dataclass
-from functools import partial
 from typing import Any, Dict, Optional, Union
 
 import mlx.core as mx
@@ -31,6 +30,11 @@ class ModelArgs(BaseModelArgs):
     tie_word_embeddings: bool
     rope_traditional: bool = False
     rope_scaling: Optional[Dict[str, Union[float, str]]] = None
+    rope_parameters: Optional[Dict] = None
+
+    def __post_init__(self):
+        if self.rope_parameters is not None:
+            self.rope_theta = self.rope_parameters.get("rope_theta", 10000.0)
 
 
 class ApertusMLP(nn.Module):
