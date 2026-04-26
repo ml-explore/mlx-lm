5804ef2d65e6b9243e07563e9c09ad2bd2af8118
Frederic Haddad <freddy@coddict.com>
2026-04-27 02:15:36 +0400

    utils: _parse_size accepts K/T/B/KB/TB suffixes

--- mlx_lm/utils.py
+++ mlx_lm/utils.py
@@ -58,7 +58,18 @@ MAX_FILE_SIZE_GB = 5
 
 
 def _parse_size(x):
-    sizes = {"M": 1e6, "G": 1e9, "MB": 1e6, "GB": 1e9, "": 1}
+    sizes = {
+        "K": 1e3,
+        "M": 1e6,
+        "G": 1e9,
+        "T": 1e12,
+        "B": 1,
+        "KB": 1e3,
+        "MB": 1e6,
+        "GB": 1e9,
+        "TB": 1e12,
+        "": 1,
+    }
     split = 0
     for xi in x:
         if not (xi.isdigit() or xi == "."):
