3deff3229d832ef861cf6f5c032989c614b868ff
AirRunner <luca.vaio@efrei.net>
2026-05-07 16:29:59 +0200

    quality: replace import functools with from functools import partial

--- mlx_lm/generate.py
+++ mlx_lm/generate.py
@@ -3,7 +3,6 @@
 import argparse
 import contextlib
 import copy
-import functools
 import json
 import math
 import sys
@@ -11,6 +10,7 @@ import time
 import warnings
 from collections import deque
 from dataclasses import asdict, dataclass
+from functools import partial
 from typing import Any, Callable, Generator, List, Optional, Sequence, Tuple, Union
 
 import mlx.core as mx
@@ -389,7 +389,7 @@ def generate_step(
 
     prompt_progress_callback = prompt_progress_callback or (lambda *_: None)
 
-    quantize_cache_fn = functools.partial(
+    quantize_cache_fn = partial(
         maybe_quantize_kv_cache,
         quantized_kv_start=quantized_kv_start,
         kv_group_size=kv_group_size,
@@ -553,7 +553,7 @@ def speculative_generate_step(
 
     sampler = sampler or greedy_sampler
 
-    quantize_cache_fn = functools.partial(
+    quantize_cache_fn = partial(
         maybe_quantize_kv_cache,
         quantized_kv_start=quantized_kv_start,
         kv_group_size=kv_group_size,
@@ -734,7 +734,7 @@ def mtp_generate_step(
         else ([], None)
     )
 
-    quantize_cache_fn = functools.partial(
+    quantize_cache_fn = partial(
         maybe_quantize_kv_cache,
         quantized_kv_start=quantized_kv_start,
         kv_group_size=kv_group_size,
