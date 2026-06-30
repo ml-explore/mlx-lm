0a48b72af41b7a6806f5f78ed73a8bbb1c5a7fec
Utkarsh Tiwari <utkarshtiwari2412@gmail.com>
2026-07-01 00:27:40 +0530

    Fix frozen PRNG in categorical_sampling under repeated sampling

--- mlx_lm/sample_utils.py
+++ mlx_lm/sample_utils.py
@@ -274,7 +274,6 @@ def apply_xtc(
     )
 
 
-@partial(mx.compile, inputs=mx.random.state, outputs=mx.random.state)
 def categorical_sampling(logits, temp):
     return mx.random.categorical(logits * (1 / temp))
 
