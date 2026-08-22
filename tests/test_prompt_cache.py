5005c8086e4244538bf255f3391fa1d5329fa75f
Cheng <git@zcbenz.com>
2026-08-22 16:41:42 +0900

    Return left_padding/lengths as cache state

--- tests/test_prompt_cache.py
+++ tests/test_prompt_cache.py
@@ -1,6 +1,7 @@
 # Copyright © 2024 Apple Inc.
 
 import copy
+import io
 import os
 import tempfile
 import unittest
@@ -758,26 +759,19 @@ class TestPromptCache(unittest.TestCase):
         self.assertEqual(stepwise[0].shape, (4, 4, 8))
         self.assertEqual(stepwise[1].shape, (4, 4))
 
-    def test_arrays_cache_advance_evaluates_metadata_with_state(self):
+    def test_arrays_cache_advance(self):
         cache = ArraysCache(2, left_padding=[2])
         cache.prepare(lengths=[3])
-        cache[0] = mx.array([0])
-        cache[1] = mx.array([0])
 
         for _ in range(256):
-            cache[0] = cache[0] + 1
             cache.advance(1)
-            mx.eval(cache[0])
+            mx.eval(cache.state)
 
-        for name, metadata in (
-            ("lengths", cache.lengths),
-            ("left-padding", cache.left_padding),
-        ):
-            dot_path = os.path.join(self.test_dir, f"arrays-cache-{name}.dot")
-            mx.export_to_dot(dot_path, metadata)
-            with open(dot_path, encoding="utf-8") as graph:
-                self.assertLessEqual(graph.read().count("->"), 8)
-        self.assertEqual(cache[0].item(), 256)
+        for attr in [cache.lengths, cache.left_padding]:
+            f = io.StringIO()
+            mx.export_to_dot(f, attr)
+            f.seek(0)
+            self.assertEqual(f.read().count("->"), 0)
         self.assertEqual(cache.lengths.item(), 3 - 256)
         self.assertEqual(cache.left_padding.item(), 2 - 256)
 
