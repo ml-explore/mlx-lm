20f8b8cb45c62024d59f1f7728d511092cf2b2ff
Cheng <git@zcbenz.com>
2026-08-22 11:24:24 +0900

    nit

--- mlx_lm/tool_parsers/qwen3_coder.py
+++ mlx_lm/tool_parsers/qwen3_coder.py
@@ -74,10 +74,7 @@ def _convert_param_value(param_value: str, param_name: str, param_config: dict) 
             try:
                 return json.loads(param_value)
             except json.JSONDecodeError:
-                try:
-                    return ast.literal_eval(param_value)
-                except (ValueError, SyntaxError):
-                    return param_value
+                pass
 
         try:
             return ast.literal_eval(param_value)
