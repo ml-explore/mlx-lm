22a8bfafd067f5514b90574229b380e6fa813e7a
Cheng <git@zcbenz.com>
2026-09-04 14:43:03 +0900

    Clear model and prompt cache in generation thread

--- mlx_lm/server.py
+++ mlx_lm/server.py
@@ -1,6 +1,7 @@
 # Copyright © 2023-2024 Apple Inc.
 
 import argparse
+import gc
 import json
 import logging
 import pickle
@@ -864,6 +865,10 @@ class ResponseGenerator:
                         # It may have already been removed during
                         # generation
                         batch_results.pop(uid, None)
+
+        del self.model_provider
+        del self.prompt_cache
+        gc.collect()
 
     def _serve_single(self, request):
         rqueue, request, args = request
